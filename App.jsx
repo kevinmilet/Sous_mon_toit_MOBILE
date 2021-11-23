68ea4b1f4c53135d0b6c9483df96444ae1021a83
Kevin Milet <kevin.milet@gmail.com>
2021-11-23 16:04:54 +0100

    ...

--- App.jsx
+++ App.jsx
@@ -1,21 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StatusBar, StyleSheet, SafeAreaView } from 'react-native';
-import Home from './src/Views/Home/Home';
+import { SafeAreaView,StyleSheet, Text, View } from 'react-native';
+import NavigationMain from './src/navigation/NavigationMain';
+import Customer from './src/screens/Customer'
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Home/>
-    </SafeAreaView>
+  <SafeAreaView style={{flex: 1}}>
+    
+    <NavigationMain />
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
