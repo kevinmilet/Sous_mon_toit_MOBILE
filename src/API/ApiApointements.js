8432bd9291c8252e95386cf9b967ccfc8f48b9b5
Kevin Milet <kevin.milet@gmail.com>
2021-11-24 14:31:46 +0100

    ...

--- src/API/ApiApointements.js
+++ src/API/ApiApointements.js
@@ -1,53 +1,46 @@
 import axios from 'axios';
 import ApiRoutes from '../utils/const/ApiRoutes';
-
-const API_URL = ApiRoutes.api_url;
+import { API_URL } from '@env';
 
 // Récuperer tous les rdvs de l'agent
 export async function getAllStaffAptmt(id) {
-    const url = `${API_URL + ApiRoutes.staff_aptmt}/${id}`;
-
+    const url = `${API_URL}/schedule/staff/${id}`;
     return await axios.get(url);
 }
 
 // Récuperer les rdvs d'un client
 export async function getAllCustomerAptmt(id) {
-    const url = `${API_URL + ApiRoutes.customer_aptmt}/${id}`;
-
+    const url = `${API_URL}/schedule/customer/${id}`;
     return await axios.get(url);
 }
 
 // Ajouter un rdv
 export async function createAptmt(values) {
-    const url = API_URL + ApiRoutes.create_aptmt;
-
+    const url = `${API_URL}/schedule/createAppt`;
     return await axios.post(url, values);
 }
 
 // Editer un rdv
 export async function updateAptmt(id, values) {
-    const url = `${API_URL + ApiRoutes.update_aptmt}/${id}`;
+    const url = `${API_URL}/schedule/update/${id}`;
 
     return await axios.put(url, values);
 }
 
 // Supprimer un rdv
 export async function deleteAptmt(id) {
-    const url = `${API_URL + ApiRoutes.delete_aptmt}/${id}`;
-
+    const url = `${API_URL}/schedule/delete/${id}`;
     return await axios.post(url);
 }
 
 // Récuperer tout les rdv du jour pour l'agent
 export async function getTodayStaffAptmts(id) {
-    const url = `${API_URL + ApiRoutes.today_s_aptmts}/${id}`;
-
+    const url = `${API_URL}/schedule/today_staff/${id}`;
     return await axios.get(url);
 }
 
 // Récuperer tout les rdv du jour pour le client
 export async function getTodayCustomerAptmts(id) {
-    const url = `${API_URL + ApiRoutes.today_c_aptmts}/${id}`;
-
+    const url = `${API_URL}/schedule/today_customer/${id}`;
     return await axios.get(url);
 }
