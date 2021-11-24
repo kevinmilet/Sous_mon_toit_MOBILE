cf0001904684484441466ddbfa9ac3c3c100b9b1
Kevin Milet <kevin.milet@gmail.com>
2021-11-24 14:14:54 +0100

    Ajout react-native-dotenv + ajout .env et modif ApiStaff.js pour tester

--- src/API/ApiStaff.js
+++ src/API/ApiStaff.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
 import ApiRoutes from '../utils/const/ApiRoutes';
+import { API_URL } from '@env';
 
-const API_URL = ApiRoutes.api_url;
+// const API_URL = ApiRoutes.api_url;
 
 // Methode de connexion
 export async function login(values) {
-    const url = API_URL + ApiRoutes.login;
+    const url = `${API_URL}/login/staff`;
 
     return await axios.post(url, values);
 }
 
 // Methode de test pour récupérer l'avatar -- NE PAS GARDER EN L'ETAT!!!
 export async function getStaffPicture(id) {
-    const url = `${API_URL + ApiRoutes.staff}/${id}`;
+    const url = `${API_URL}/login/staff${id}`;
 
     return await axios.get(url);
         
 }
 
-export const AVATAR_BASE_URL = 'http://api-sousmontoit.am.manusien-ecolelamanu.fr/storage/app/public/pictures/avatars/';
+// export const AVATAR_BASE_URL = 'http://api-sousmontoit.am.manusien-ecolelamanu.fr/storage/app/public/pictures/avatars/';
