cb8e3e75ad264914f78682b38e96a853c3dd8bb8
AnneCrepin <anne.crepin62@gmail.com>
2021-11-25 13:41:22 +0100

    modif estate

--- src/components/Estate/EstateCard.jsx
+++ src/components/Estate/EstateCard.jsx
@@ -1,18 +1,33 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React , {useEffect, useState} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View , Image} from 'react-native';
+import { getEstateCover} from '../../API/ApiEstates';
+import { COVER_ESTATE_BASE_URL } from '@env';
 
 const EstateCard = (props) => {
 
     const { estate } = props;
+    const [pictureCover, setPictureCover] = useState({})
     const navigation = useNavigation();
+
+    useEffect(() => {
+        //Image de couverture du bien
+        getEstateCover(estate.id)
+            .then(res => {
+                setPictureCover(res.data[0])
+            }).catch(error => {
+                console.log(error.message)
+            })
+    }, [estate])
 
     return (
 
-        <TouchableOpacity style={styles.main_container} onPress={() => navigation.navigate('estateDetail')} >
+        <TouchableOpacity style={styles.main_container} onPress={() => navigation.navigate('estateDetail', { estateId: estate.id })} >
             <View style={styles.content_container}>
                 <View >
                     <Text>{estate.title}</Text>
+                    <Text>{COVER_ESTATE_BASE_URL + pictureCover.name}</Text>
+                    <Image source={{ uri: COVER_ESTATE_BASE_URL + pictureCover.name }} style={{height:100, width:100}}/>
                 </View>
             </View>
         </TouchableOpacity>
