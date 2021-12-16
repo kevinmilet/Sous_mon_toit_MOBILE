59ebb9398bc3547caa0596c21334f6873be82746
laurentvac <laurentvac80@gmail.com>
2021-12-16 16:56:49 +0100

    ajout searchbar customer

--- src/components/Customer/CustomerList.jsx
+++ src/components/Customer/CustomerList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, TextInput, Modal, Pressable, Text, TouchableOpacity, Button} from 'react-native';
 import { getAllCustomers } from "../../API/ApiCustomers";
 import Card from '../../components/Customer/Card';
 import Loader from "../../Tools/Loader/Loader";
@@ -7,6 +7,7 @@ import Topbar from './../Topbar/Topbar';
 import colors from '../../utils/styles/colors';
 import { Searchbar } from 'react-native-paper';
 import {searchCustomers} from "../../API/ApiCustomers";
+import { useNavigation } from '@react-navigation/native';
 
 
 const CustomerList = () => {
@@ -19,6 +20,13 @@ const CustomerList = () => {
     const [modalEVisible, setModalEVisible] = useState(false);
     const [customerData2, setCustomerData2] = useState([]);
     const [customer, setCustomer] = useState('');
+    const navigation = useNavigation();
+    const selectCustomer = (customerIT) => {
+        setCustomer(customerIT);
+        setModalCVisible(false)
+        setCustomerData2(null);
+        setCustomerInput(null);
+    }
 
     useEffect(() => {
         getAllCustomers().then((res) => {
@@ -38,6 +46,10 @@ const CustomerList = () => {
             };
         
              getCustomersResults = (text) => {
+                 if(!text){
+                     alert('cc')
+                     getAllCustomers();
+                 }
                 searchCustomers(text).then(
                     response => {
                         setCustomerData2(response.data);
@@ -48,7 +60,11 @@ const CustomerList = () => {
                 ).catch(error => {
                     console.log(error.message);
                 })
+            }; 
+            onPress1 = () => {
+                setCustomer(null);
             }
+          
     }, []);
 
     if (loading) {
@@ -74,15 +90,75 @@ const CustomerList = () => {
                                    );
                                }}
                                value={customerInput} />
-                    <View>
-                        <TextInput editable={false}>{customer.firstname} {customer.lastname}</TextInput>
-                    </View>
-        <FlatList
-            data={customerData1}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({item}) => <Card customer={item}/>}
-        
-        />
+                               
+                    <Modal animationType="slide"
+                            transparent={true}
+                            visible={modalCVisible}
+                            onRequestClose={() => {
+                               setCustomerData(null);
+                               setSearchTimer(null);
+                               setModalCVisible(false);
+                            }}
+                            >
+                        <View style={styles.centeredModal}>
+                            <View style={styles.modalView}>
+                                <FlatList
+                                          data={customerData2}
+                                          renderItem={({ item }) => (
+                                              <View>
+                                                  <TouchableOpacity onPress={() => selectCustomer(item)}>
+                                                        <Text style={styles.modalText}>{item.firstname} {item.lastname}</Text>
+                                                  </TouchableOpacity>
+                                              </View>
+                                          )}
+                                          keyExtractor={(item) => "" + item.id}
+                                />
+                            </View>
+                            <Pressable
+                                style={[styles.modalButton, styles.buttonClose]}
+                                onPress={() => setModalCVisible(false)}
+                            >
+                                <Text style={styles.btnText}>Fermer</Text>
+                            </Pressable>
+                        </View>
+                    </Modal>
+                    {!customer  ?(
+                        <FlatList
+                        data={customerData1}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({item}) => <Card customer={item}/>}
+
+                        />
+                      
+                      
+                     
+                    ):( 
+                        <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('customerDetail', {id : customer.id})} > 
+                        <View style={styles.content_container}>  
+                          <View >
+                          <Text style={styles.baseText}>
+                              Nom :
+                              <Text style={styles.innerText}> {customer.lastname}</Text>
+                            </Text>
+                            <Text style={styles.baseText}>
+                              Prénom :
+                              <Text style={styles.innerText}> {customer.firstname}</Text>
+                            </Text>
+                            <Text style={styles.baseText}>
+                              Mail :<Text style={styles.innerText}> {customer.mail}</Text>
+                            </Text>
+                          
+                          </View>
+                        </View>
+                      </TouchableOpacity>
+                        
+                    //     <View>
+                    //     <TextInput editable={false}>{customer.firstname} {customer.lastname}</TextInput>
+                    //    </View>
+                    )
+                }
+                   
+                   <Button title="Tout les clients" onPress={onPress1} /> 
         </View>
     );
 };
@@ -91,10 +167,181 @@ const styles =  StyleSheet.create({
         flex: 1,
         backgroundColor: colors.primary
     },
+    card: {
+     
+        margin: 10,
+        padding: 10,
+        backgroundColor: colors.secondaryBtn,
+        borderRadius: 20
+      },
     loading_container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    datetime_container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    dateInput: {
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderColor: colors.secondaryBtn,
+        borderRadius: 25,
+        borderWidth: 1,
+        width: 200,
+        marginVertical: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 55,
+        fontSize: 18,
+        backgroundColor: colors.backgroundPrimary,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    timeInput: {
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderColor: colors.secondaryBtn,
+        borderRadius: 25,
+        borderWidth: 1,
+        width: 100,
+        marginVertical: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 55,
+        fontSize: 18,
+        backgroundColor: colors.backgroundPrimary,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    textareaInput: {
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderColor: colors.secondaryBtn,
+        borderRadius: 25,
+        borderWidth: 1,
+        width: '100%',
+        marginVertical: 5,
+        fontSize: 18,
+        backgroundColor: colors.backgroundPrimary,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    // Seulement pour iOS
+    datePicker: {
+        width: 320,
+        height: 260,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+    dropdown : {
+        borderRadius: 25
+    },
+    dropdownInput : {
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderColor: colors.secondaryBtn,
+        borderRadius: 25,
+        borderWidth: 1,
+        width: '100%',
+        marginVertical: 5,
+        height: 55,
+        fontSize: 18,
+        backgroundColor: colors.backgroundPrimary,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    selectBtnText: {
+        color: colors.primary,
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: 'normal'
+    },
+    button: {
+        padding: 10,
+        borderRadius: 25,
+        backgroundColor: colors.primaryBtn,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginVertical: 10,
+        height: 55,
+        width: 245,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    btnText: {
+        color: colors.backgroundPrimary,
+        textAlign: 'center',
+        fontSize: 18,
+    },
+    centeredModal: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        width: '80%',
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 10,
+        padding: 35,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    modalButton: {
+        elevation: 2,
+        padding: 10,
+        borderRadius: 25,
+        backgroundColor: colors.secondaryBtn,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginVertical: 10,
+        height: 50,
+        width: 150,
+    },
+    modalText: {
+        marginBottom: 20,
+        textAlign: "left",
+        fontSize: 18,
     }
 })
 
